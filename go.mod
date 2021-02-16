ec4f6d1464fa6641362d34f8a0728c8b99e23bd8
Ben Grewell <BGrewell@gmail.com>
2021-02-16 14:02:07 -0800

    add file update code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/BGrewell/go-update-test
 
 go 1.15
 
-require github.com/mcuadros/go-version v0.0.0-20190830083331-035f6764e8d2
+require (
+	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf
+	github.com/mcuadros/go-version v0.0.0-20190830083331-035f6764e8d2
+)
--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"errors"
 	"fmt"
 	vercheck "github.com/mcuadros/go-version"
+	"github.com/inconshreveable/go-update"
+	"io"
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 var (
 	version = "DEBUG"
 	tagCheckUrl = "https://github.com/BGrewell/go-update-test/releases/latest"
-	updateUrlStub = "https://github.com/BGrewell/go-update-test/releases/download/%VER%/go-update-test_%VER%_Linux_x86_64.tar.gz"
+	updateUrlStub = "https://github.com/BGrewell/go-update-test/releases/download/%VER1%/go-update-test_%VER2%_Linux_x86_64.tar.gz"
 )
 
 func latestTaggedVersion() string {
@@ -40,6 +46,43 @@ func isUpdateAvailable(onlineVersion string) bool {
 	return vercheck.Compare(onlineVersion, version, ">")
 }
 
+func getChecksums() string {
+	return ""
+}
+
+func getPackage(version string) io.Reader {
+	url := strings.ReplaceAll(updateUrlStub, "%VER1%", version)
+	url = strings.ReplaceAll(url, "%VER2%", strings.Replace(version, "v", "", 1))
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("failed to get update package: %v\n", err)
+	}
+
+	defer resp.Body.Close()
+	gzr, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		log.Fatalf("failed to remove gzip encoding: %v\n", err)
+	}
+	tr := tar.NewReader(gzr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to extract binary from package: %v\n", err)
+		}
+		fmt.Printf("file: %s\n", hdr.Name)
+		if !strings.Contains(hdr.Name, ".md") {
+			var filebuf bytes.Buffer
+			io.TeeReader(tr, &filebuf)
+			return &filebuf
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	fmt.Println("[+] This is a simple program to test out auto-updating go applications")
 	fmt.Println("[+] it will check for an update and if one is available it will apply it")
@@ -48,9 +91,18 @@ func main() {
 	githubVersion := latestTaggedVersion()
 	fmt.Printf("[!] Latest tagged version on GitHub: %s\n", githubVersion)
 	updateAvailable := isUpdateAvailable(githubVersion)
-	update := "Update Available"
+	updateStatus := "Update Available"
 	if !updateAvailable {
-		update = "Running latest version"
+		updateStatus = "Running latest version"
 	}
-	fmt.Printf("[!] %s\n", update)
+	fmt.Printf("[!] %s\n", updateStatus)
+	if updateAvailable {
+		binary := getPackage(githubVersion)
+		options := update.Options{}
+		err := update.Apply(binary, options)
+		if err != nil {
+			log.Fatalf("failed to apply update: %v", err)
+		}
+		fmt.Println("[!] Applied update")
+	}
 }
